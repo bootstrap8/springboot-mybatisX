1812c9fda5314b2d03642f4639282aaefb5792d8
zhaoyy8 <stone@malou123>
2021-09-02 19:47:13 +0800

    [*] init

---
+++ src/main/java/com/fit/spirngbootmybatisX/SpirngbootMybatisXApplication.java
@@ -0,0 +1,13 @@
+package com.fit.spirngbootmybatisX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpirngbootMybatisXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpirngbootMybatisXApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fit/spirngbootmybatisX/SpirngbootMybatisXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fit.spirngbootmybatisX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpirngbootMybatisXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
